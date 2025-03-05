934d1d8cb35090c9b11075d2e6c4e573e34b2ab5
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-03-05 15:53:13 +0530

    [Adarsh] Add. main.js

---
+++ AddressBook/main.js
@@ -0,0 +1,26 @@
+const Contact = require("./contact");
+
+let contact1 = new Contact(
+    "Adarsh",
+    "Patel",
+    "123 Main st",
+    "SpringField",
+    "ILkm",
+    "62704",
+    "1234567890",
+    "adarshpatel@gmail.com"
+);
+
+let contact2 = new Contact(
+    "Uday",
+    "Kourav",
+    "234 Main vt",
+    "Boston",
+    "Makm",
+    "02108",
+    "9876543210",
+    "udaykourav@example.com"
+);
+
+console.log("Contact1: ", contact1);
+console.log("Contact2: ", contact2);
