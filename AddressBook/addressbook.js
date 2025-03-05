f11c63994da75a6ee45fa3885b5d80e0f8fb81c3
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-03-05 16:56:44 +0530

    [Adarsh] Update. method to edit details

--- AddressBook/addressbook.js
+++ AddressBook/addressbook.js
@@ -10,6 +10,20 @@ class AddressBook {
     displayContacts() {
         return this.contacts.map((contact) => contact.toString()).join("\n");
     }
+
+    editDetails(firstName, lastName, newDetails) {
+        let contact = this.contacts.find(contact => 
+            contact.firstName.toLowerCase()===firstName.toLowerCase() &&
+            contact.lastName.toLowerCase()===lastName.toLowerCase()
+        );
+
+        if(contact) {
+            Object.assign(contact, newDetails);
+            console.log("Contact Updated Successfully: \n", contact);
+        } else {
+            console.log("Contact Not Found");
+        }
+    }
 }
 
 module.exports = AddressBook;
